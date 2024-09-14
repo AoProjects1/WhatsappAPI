1baba32e30729b945bf6afeebf7b61137361e41c
Abdelrhmanm7md <abdelrahmanmohammed851@gmail.com>
2024-09-14 18:33:32 +0300

    14/9/2024

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 dbConnection();
-app.use(globalError);
 app.use(messageRouter);
 app.use("/", (req, res, next) => {
   res.send("Page Not Found");
@@ -28,5 +27,6 @@ app.use("/", (req, res, next) => {
 app.all("*", (req, res, next) => {
   next(res.status(404).json({ message: "Not found" }));
 });
+app.use(globalError);
 startClient(4);
 app.listen(3000, () => console.log(`Server is ready in on port 3000`));
