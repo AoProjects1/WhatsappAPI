db29ee3a9affc41c5d9c4bdc7af49ccbdb7b37de
Abdelrhmanm7md <abdelrahmanmohammed851@gmail.com>
2024-09-29 19:33:50 +0300

    **

--- src/modules/message.controller.js
+++ src/modules/message.controller.js
@@ -1,6 +1,6 @@
 import { userOrderModel } from "../database/models/userOrder.model.js";
 import { startClient, sendMessage } from "../services/WhatsappClient.js";
-import cron from 'node-cron';
+import cron from "node-cron";
 import catchAsync from "../utils/globalError.js";
 
 const createMessage = catchAsync(async (req, res, next) => {
@@ -10,61 +10,81 @@ const createMessage = catchAsync(async (req, res, next) => {
   sendMessage(req.body.phoneNumber, req.body.message, clientId);
 
   let { phoneNumber, orderId } = req.body;
-  const dateWithTimeZone = (timeZone, year, month, day, hour, minute, second) => {
+  const dateWithTimeZone = (
+    timeZone,
+    year,
+    month,
+    day,
+    hour,
+    minute,
+    second
+  ) => {
     let date = new Date(Date.UTC(year, month, day, hour, minute, second));
-    return new Date(date.toLocaleString('en-US', { timeZone: timeZone }));
+    return new Date(date.toLocaleString("en-US", { timeZone: timeZone }));
   };
-  
+
   const date = new Date();
-  date.setMinutes(date.getMinutes() );
+  date.setMinutes(date.getMinutes());
   // date.setHours(date.getHours() + 8);
 
   const year = date.getUTCFullYear();
-  const month = date.getUTCMonth() ; 
+  const month = date.getUTCMonth();
   const day = date.getUTCDate();
   const hour = date.getUTCHours();
   const minute = date.getUTCMinutes();
   const second = date.getUTCSeconds();
-  let egyptDate = dateWithTimeZone('Africa/Cairo', year, month, day, hour, minute, second);
+  let egyptDate = dateWithTimeZone(
+    "Africa/Cairo",
+    year,
+    month,
+    day,
+    hour,
+    minute,
+    second
+  );
 
-let newUserOrder = new userOrderModel({
-   phoneNumber ,
-   orderId,
-   clientId,
-   date: egyptDate,
-   responsed: false
- })
- await newUserOrder.save()
+  let newUserOrder = new userOrderModel({
+    phoneNumber,
+    orderId,
+    clientId,
+    date: egyptDate,
+    responsed: false,
+  });
+  await newUserOrder.save();
   res.status(201).json({
     message: "User Order created successfully!",
     newUserOrder,
   });
 
-let cairoDate = egyptDate
-cairoDate.setMinutes(cairoDate.getMinutes() +1 );
- console.log(cairoDate,"ca");
- 
-  const cronExpression = `${cairoDate.getMinutes()} ${cairoDate.getHours()} * * *`;  
-const task = cron.schedule(cronExpression, async () => {
-  try {
-    let check = await userOrderModel.findOne({ _id: newUserOrder._id });
-    if (check && check.responsed === false) {
-      await userOrderModel.findByIdAndUpdate(newUserOrder._id, { responsed: true });
-      sendMessage(newUserOrder.phoneNumber, `Your order is cancelled due to Time OUT`, newUserOrder.clientId);
-      console.log(`Order ${newUserOrder.orderId} cancelled due to timeout`);
+  let cairoDate = date;
+  cairoDate.setMinutes(cairoDate.getMinutes() + 1);
+  console.log(cairoDate, "ca");
+
+  const cronExpression = `${cairoDate.getMinutes()} ${cairoDate.getHours()} * * *`;
+  const task = cron.schedule(cronExpression, async () => {
+    try {
+      let check = await userOrderModel.findOne({ _id: newUserOrder._id });
+      if (check && check.responsed === false) {
+        await userOrderModel.findByIdAndUpdate(newUserOrder._id, {
+          responsed: true,
+        });
+        sendMessage(
+          newUserOrder.phoneNumber,
+          `Your order is cancelled due to Time OUT`,
+          newUserOrder.clientId
+        );
+        console.log(`Order ${newUserOrder.orderId} cancelled due to timeout`);
+      }
+      task.stop();
+    } catch (error) {
+      console.error("Error during cron job execution:", error);
     }
-    task.stop();
-  } catch (error) {
-    console.error("Error during cron job execution:", error);
-  }
   });
-  });
-
-
+});
 
 const createClient = catchAsync(async (req, res, next) => {
-    startClient(req.params.id);
-    res.send();
-  });
+  startClient(req.params.id);
+  res.send();
+});
 
-  export {createMessage , createClient}
+export { createMessage, createClient };
